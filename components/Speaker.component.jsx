a53533ff68473f845966ffaa6f808730a1a81831
OliverMengich <kemeioliver2000@gmail.com>
2023-07-19 22:27:14 +0300

    Speaker Item Modifications

--- components/Speaker.component.jsx
+++ components/Speaker.component.jsx
@@ -4,23 +4,31 @@ function SpeakerComponent(props) {
     return (
         <View style={styles.itemContainer}>
             <Image source={require('../assets/icon.png')} style={{width: 60, height: 60, borderRadius: 20}}/>
-            <Text style={styles.boldText}>Speaker</Text>
+            <Text style={styles.boldText}>Oliver Kipkemei</Text>
             <Text style={styles.normalText}>Web Developer @Microsoft</Text>
-            <Pressable android_ripple={{color:'#f5f5f5'}}  style={{backgroundColor: '#4285f4', width: '100%',alignItems:'center', paddingVertical: 10}}>
-                <Text style={{color: '#fff'}}>Hello</Text>
+            <Pressable android_ripple={{color:'#f5f5f5'}}  style={{backgroundColor: '#4285f4', width: '100%',alignItems:'center',marginVertical: 10, paddingVertical: 10}}>
+                <Text style={{color: '#fff'}}>View</Text>
             </Pressable>
         </View>
     );
 }
 const styles = StyleSheet.create({
-    itemContainer: {backgroundColor: '#fff', width: '40%',marginHorizontal:5, borderRadius: 4,paddingVertical:6, alignItems: 'center'},
+    itemContainer: {
+        backgroundColor: '#fff', 
+        width: '100%',
+        marginHorizontal:5, 
+        borderRadius: 7,
+        padding:16, 
+        alignItems: 'center'
+    },
     boldText:{
         fontWeight: 'bold',
-        fontSize: 20,
+        fontSize: 16,
     },
     normalText:{
-        fontSize: 10,
+        fontSize: 12,
         color: '#666',
+        marginBottom: 10,
     }
 });
 
