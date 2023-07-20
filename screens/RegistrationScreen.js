e36d0f704eced6846d98562891632de01cd5c952
OliverMengich <kemeioliver2000@gmail.com>
2023-07-20 16:22:39 +0300

    Registration logic

--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -1,8 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet,SafeAreaView, Image, Text, View, Dimensions, Pressable, TextInput } from 'react-native';
 import Icon  from 'react-native-vector-icons/MaterialCommunityIcons';
-
-export default function RegistrationScreen() {
+import { useState } from 'react';
+import { useQuery, } from '@tanstack/react-query';
+export default function RegistrationScreen({navigation}) {
+    function handleNavigation(){
+        navigation.navigate('Login');
+    }
+    const [registrationData, setRegistrationData] = useState({
+        email: '',
+        password: '',
+        name: '',
+        confirPassword: ''
+    });
+    const { refetch, data} = useQuery({
+        queryKey: ['register'],
+        queryFn:  ()=>{
+            console.log('Sending connection login')
+            return axios.post('http://192.168.88.251:3000/register',{
+                email: registrationData.email,
+                password: registrationData.password,
+                name: registrationData.name,
+            }).then((response)=>{
+                console.log(response.data);
+                navigation.navigate('Login');
+                return response.data;
+            }).catch((error)=>{
+                console.log('Error encountered',error);
+            });
+        },
+        enabled: false,
+        networkMode: 'offline',
+    })
+    const handleTextChange = (key,value) => {
+        console.log(key,value);
+        setRegistrationData({
+            ...registrationData,
+            [ key ]: value,
+        });
+    }
+    function handleRegistration(){
+        console.log('Logging in');
+        console.log(registrationData);
+        refetch();
+    }
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar  style='auto' />
@@ -10,21 +51,40 @@ export default function RegistrationScreen() {
             <View style={{width: '100%', alignItems:'center'}}>
                 <View style={{width:'100%'}}>
                     <Text>Email</Text>
-                    <TextInput keyboardType='email-address' style={styles.textInputStyle} placeholder='Enter Your email'/>
+                    <TextInput 
+                        keyboardType='email-address' 
+                        style={styles.textInputStyle} 
+                        placeholder='Enter Your email'
+                        onChangeText={(text)=>handleTextChange('email',text)} 
+                    />
                 </View> 
                 <View style={{width:'100%'}}>
                     <Text>Name</Text>
-                    <TextInput style={styles.textInputStyle} placeholder='Enter Your Name'/>
+                    <TextInput 
+                        style={styles.textInputStyle} 
+                        placeholder='Enter Your Name'
+                        onChangeText={(text)=>handleTextChange('name',text)} 
+                    />
                 </View> 
                 <View style={{width:'100%'}}>
                     <Text>Password</Text>
-                    <TextInput style={styles.textInputStyle} placeholder='Enter Your Password'/>
+                    <TextInput 
+                        keyboardType='visible-password'
+                        style={styles.textInputStyle} 
+                        placeholder='Enter Your Password'
+                        onChangeText={(text)=>handleTextChange('password',text)} 
+                    />
                 </View>
                 <View style={{width:'100%'}}>
                     <Text>Confirm Password</Text>
-                    <TextInput style={styles.textInputStyle} placeholder='Retype Your Password'/>
+                    <TextInput 
+                        style={styles.textInputStyle} 
+                        placeholder='Retype Your Password'
+                        keyboardType='visible-password'
+                        onChangeText={(text)=>handleTextChange('confirmPassword',text)} 
+                    />
                 </View>    
-                <Pressable style={styles.buttonStyle}>
+                <Pressable onPress={handleRegistration} style={styles.buttonStyle}>
                     <Text style={styles.textcolor}>REGISTER</Text>
                 </Pressable>
                 
@@ -35,7 +95,7 @@ export default function RegistrationScreen() {
                     <Icon size={25} name="linkedin" style={styles.signInOptionIcon} color={'#24a3f1'} />
 
                 </View>
-                <Pressable>
+                <Pressable onPress={handleNavigation}>
                     <Text style={styles.forgotPasswordText}>Already have an account? Login.</Text>
                 </Pressable>
             </View>
