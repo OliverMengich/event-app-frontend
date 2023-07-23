39514944c6ab9b6eb1ca2a15da315e39d43d97a9
OliverMengich <kemeioliver2000@gmail.com>
2023-07-23 22:18:06 +0300

    Speaker profile page mod

--- screens/SpeakerProfile.jsx
+++ screens/SpeakerProfile.jsx
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet,SafeAreaView,Switch, Image, Text, View, Dimensions, Pressable, ImageBackground } from 'react-native';
 import Icon  from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useQuery } from '@tanstack/react-query';
+import { CONSTS } from '../constants';
+import { FlatList } from 'react-native-gesture-handler';
+import EventItemComponent from '../components/EventItem.component';
 const {width, height} = Dimensions.get('window');
 // import AnimatedLoader from 'react-native-animated-loader'
 export default function SpeakerProfile({route}) {
@@ -45,42 +48,58 @@ export default function SpeakerProfile({route}) {
                     <View style={{marginVertical: 10}}>
                         <Text style={{alignSelf: 'center'}}>{data.name}</Text>
                         <Text style={styles.boldText}>{data.role}</Text>
-                        <Pressable android_ripple={{color:'#f5f5f5'}}  style={styles.butttonStyle}>
-                            <Text style={{color: 'white'}}>Edit Profile</Text>
+                        <Text>{data.description}</Text>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <Pressable android_ripple={{color:'#f5f5f5'}}  style={styles.butttonStyle}>
+                                <Text style={{color: 'white'}}>FOLLOW</Text>
+                            </Pressable>
+                            <Pressable android_ripple={{color:'#f5f5f5'}}  style={styles.messageButtonStyle}>
+                                <Text style={{color: 'white'}}>MESSAGE</Text>
+                            </Pressable>
+                        </View>
+                    </View>
+                </View>
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <Text style={styles.boldText}>Events</Text>
+                    <Pressable android_ripple={{color: '#f5f5f5'}}>
+                        <Text style={{color: CONSTS.PRIMARY_COLOR}}>Show All</Text>
+                    </Pressable>
+                </View>
+                <FlatList
+                    data={data.events}
+                    renderItem={({item}) => (
+                        <EventItemComponent
+                            date={item.date}
+                            handleNavigation={()=>{}}
+                            title={item.title}
+                            description={item.description}
+                            id={item.id}
+                            location={item.location}
+                            price={item.price}
+                            image={item.image}
+                        />
+                    )}
+                    keyExtractor={item => item.id}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    style={{marginVertical: 10}}
+                />
+                <View>
+                    <Text>Socials</Text>
+                    <View style={styles.row}>
+                        <Pressable style={styles.signInOptionIcon} android_ripple={{color: '#f5f5f5'}}>
+                            <Icon color={CONSTS.PRIMARY_COLOR} size={25} name="facebook" />
+                        </Pressable>
+                        <Pressable style={styles.signInOptionIcon} android_ripple={{color: '#f5f5f5'}}>
+                            <Icon color={CONSTS.PRIMARY_COLOR} size={25} name="twitter" />
+                        </Pressable>
+                        <Pressable style={styles.signInOptionIcon} android_ripple={{color: '#f5f5f5'}}>
+                            <Icon color={CONSTS.PRIMARY_COLOR} size={25} name="instagram" />
+                        </Pressable>
+                        <Pressable style={styles.signInOptionIcon} android_ripple={{color: '#f5f5f5'}}>
+                            <Icon color={CONSTS.PRIMARY_COLOR} size={25} name="linkedin" />
                         </Pressable>
                     </View>
-                </View>
-            </View>
-            <View style={{width: '80%'}}>
-                <View style={styles.detailsRole}>
-                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                        <Icon name="account" size={20} color="#262739" />
-                        <Text>Edit Profile</Text>
-                    </View>
-                    <Icon name="chevron-right" size={20} color="#262739" />
-                </View>
-                <View style={styles.detailsRole}>
-                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                        <Icon name="account" size={20} color="#262739" />
-                        <Text>My Events</Text>
-                    </View>
-                    <Icon name="chevron-right" size={20} color="#262739" />
-                </View>
-                <View style={styles.detailsRole}>
-                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                        <Icon name="account" size={20} color="#262739" />
-                        <Text>Feedback</Text>
-                    </View>
-                    <Icon name="chevron-right" size={20} color="#262739" />
-                </View>
-                <View style={styles.detailsRole}>
-                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                        <Icon name="bell" size={20} color="#262739" />
-                        
-                        <Text>Notifications</Text>
-                    </View>
-                    <Switch  />
-                    {/* <Icon name="chevron-right" size={20} color="#262739" /> */}
                 </View>
             </View>
         </SafeAreaView>
@@ -90,13 +109,11 @@ export default function SpeakerProfile({route}) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        // backgroundColor: '#f9f7f8',
+        backgroundColor: '#f9f7f8',
         position: 'relative',
         // marginTop: height * .25,
         // paddingHorizontal:10,
         width: '100%',
-        alignItems: 'center'
-
     },
     tinyLogo:{
         width: width,
@@ -123,15 +140,13 @@ const styles = StyleSheet.create({
         backgroundColor: '#4285f4',
         width: width *.5,
     },
-    detailsRole: {
-        flexDirection: 'row',
-        borderWidth: 1, 
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingVertical: 10,
-        paddingHorizontal: 10,
-        borderColor: '#888',
-        marginVertical: 10,
+    messageButtonStyle:{
         borderRadius: 20,
+        paddingVertical: 10,
+        alignItems: 'center',
+        borderWidth: 1,
+        backgroundColor: '#4285f4',
+        width: width *.5,
     },
+    signInOptionIcon:{backgroundColor:'#fff', borderRadius: 15, padding:5, marginHorizontal:5}
 });
