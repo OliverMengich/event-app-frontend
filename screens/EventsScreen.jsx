1d17fe900a53150b2b011ccdad55f09f7439de79
OliverMengich <kemeioliver2000@gmail.com>
2023-07-26 09:04:31 +0300

    Fetches from cache

--- screens/EventsScreen.jsx
+++ screens/EventsScreen.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 import EventItemComponent from '../components/EventItem.component';
 import { EVENTS } from './FavouritesScreen';
+import { useQueryClient } from '@tanstack/react-query';
 function EventsScreen({route, navigation}) {
     function handleNavigation(id){
         navigation.navigate('EventDetails',{
             eventId: id
         })
     }
+    const queryClient = useQueryClient();
+    const events = queryClient.getQueryData(['events']);
+    console.log('All events',events);
     return (
         <View style={{flex: 1}}>
             <Text style={styles.boldText}>All Events</Text>
             <FlatList
-                data={EVENTS}
+                data={events}
                 renderItem={({item}) => (
                     <EventItemComponent
                         title={item.title}
